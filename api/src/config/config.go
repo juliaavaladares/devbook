2893ebdea25cc087b9ab3224b407988bf85f4717
Júlia <julia.valadares99@gmail.com>
2022-03-25 15:20:57 -0300

    feat: cria configuracoes de variaveis de ambiente

---
+++ api/src/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConexaoBanco = ""
+	Porta              = 0
+)
+
+func Carregar() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Porta, err = strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		Porta = 5000
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True", user, password, db_name)
+
+}
